cd1e9dd66b3758defe453aee3512ca48b6cadd55
Pulkit <aggarwalpulkit596@gmail.com>
2020-01-29 15:00:32 +0530

    add implicit intent

--- Lecture4/Intents/app/src/main/java/com/puldroid/intents/Main2Activity.kt
+++ Lecture4/Intents/app/src/main/java/com/puldroid/intents/Main2Activity.kt
@@ -1,8 +1,11 @@
 package com.puldroid.intents
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
@@ -10,10 +13,31 @@ class Main2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         val name = intent.getStringExtra("Name")
-        val age = intent.getIntExtra("Age",-1)
-        val gender = intent.getBooleanExtra("Gender",false)
+        val age = intent.getIntExtra("Age", -1)
+        val gender = intent.getBooleanExtra("Gender", false)
 
-        Toast.makeText(this,"$name $age $gender",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "$name $age $gender", Toast.LENGTH_SHORT).show()
+        val i = Intent()
+        button.setOnClickListener {
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("http://" + editText.text.toString())
+            startActivity(i)
+
+        }
+
+        button2.setOnClickListener {
+            i.action = Intent.ACTION_DIAL
+            i.data = Uri.parse("tel:" + editText.text.toString())
+            startActivity(i)
+
+        }
+
+        button3.setOnClickListener {
+            i.action = Intent.ACTION_SENDTO
+            i.data = Uri.parse("mailto:" + editText.text.toString())
+            startActivity(i)
+
+        }
 
     }
 }
