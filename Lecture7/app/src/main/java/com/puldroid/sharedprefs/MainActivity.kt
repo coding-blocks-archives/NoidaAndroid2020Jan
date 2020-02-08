e2b201c9a6d385354e416cb395c2f1bab58e3ce6
Pulkit <aggarwalpulkit596@gmail.com>
2020-02-08 18:45:52 +0530

    dialog with extension function

--- Lecture7/app/src/main/java/com/puldroid/sharedprefs/MainActivity.kt
+++ Lecture7/app/src/main/java/com/puldroid/sharedprefs/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.puldroid.sharedprefs
 
 import android.content.Context
+import android.content.DialogInterface
+import android.icu.text.CaseMap
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,9 +27,23 @@ class MainActivity : AppCompatActivity() {
 
         textView.append("$count")
 
+//        val dialog = AlertDialog.Builder(this)
+//            .setTitle("Simple Dialog")
+//            .setMessage("This is some sample description")
+//            .setPositiveButton("Ok") { dialogInterface: DialogInterface, i: Int ->
+//                Toast.makeText(this,"Snackbar Action",Toast.LENGTH_SHORT).show()
+//            }
+//            .setNegativeButton("Cancel") { dialogInterface: DialogInterface, i: Int ->
+//
+//            }
+//            .setCancelable(false)
+
+
+
+
 
         updateBtn.setOnClickListener {
-
+            dialog()
 //            Snackbar.make(root, "Update Preferences", Snackbar.LENGTH_LONG).show()
             root.snackbar("Update Preferences",Snackbar.LENGTH_LONG,{
                 Toast.makeText(this,"Snackbar Action",Toast.LENGTH_SHORT).show()
@@ -40,6 +57,8 @@ class MainActivity : AppCompatActivity() {
             textView.text = "Count ${count}"
 
         }
+
+
     }
 }
 
@@ -48,5 +67,19 @@ fun View.snackbar(message: String, duration: Int, function: () -> Unit) {
         .setAction("Undo") {
             function()
         }
+        .show()
+}
+
+fun Context.dialog(message: String = "This is some sample description", title: String = "Simple Dialog",cancelable:Boolean = false,function: (status:Boolean) -> Unit = {}) {
+    AlertDialog.Builder(this)
+        .setTitle(title)
+        .setMessage(message)
+        .setPositiveButton("Ok") { dialogInterface: DialogInterface, i: Int ->
+            function(true)
+        }
+        .setNegativeButton("Cancel") { dialogInterface: DialogInterface, i: Int ->
+            function(false)
+        }
+        .setCancelable(cancelable)
         .show()
 }
