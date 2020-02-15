955a6df0fcf5ce9f44ccfd71995e830809157a1a
Pulkit <aggarwalpulkit596@gmail.com>
2020-02-15 18:22:50 +0530

    add photos adapter with shapeableimageview

--- Lecture9/app/src/main/java/com/puldroid/networking/MainActivity.kt
+++ Lecture9/app/src/main/java/com/puldroid/networking/MainActivity.kt
@@ -3,13 +3,18 @@ package com.puldroid.networking
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import org.json.JSONObject
+import java.lang.reflect.Type
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,19 +25,29 @@ class MainActivity : AppCompatActivity() {
         val okHttpClient = OkHttpClient()
 
         val request = Request.Builder()
-                .url("https://jsonplaceholder.typicode.com/photos/1")
+                .url("https://jsonplaceholder.typicode.com/photos")
                 .build()
+        val list = arrayListOf<Photos>()
+        val photoAdapter = PhotoAdapter(list)
+        photosRv.apply {
+            layoutManager = GridLayoutManager(this@MainActivity,3)
+            adapter = photoAdapter
+        }
 
-        GlobalScope.launch {
+        GlobalScope.launch(Dispatchers.Main) {
             val response = withContext(Dispatchers.IO) { okHttpClient.newCall(request).execute() }
             if (response.isSuccessful) {
                 response.body?.let {
-                    val obj = JSONObject(it.string())
-                    val albumId = obj["albumId"] as Int
-                    val id = obj["id"] as Int
-                    val title = obj["title"] as String
-                    val url  = obj["url"] as String
-                    val photos = Photos(albumId,id, title, url)
+                    val collectionType: Type = object : TypeToken<List<Photos>>() {}.type
+                    val photos: List<Photos> = Gson().fromJson(it.string(), collectionType)
+                    list.addAll(photos)
+                    photoAdapter.notifyDataSetChanged()
+
+//                    val albumId = obj["albumId"] as Int
+//                    val id = obj["id"] as Int
+//                    val title = obj["title"] as String
+//                    val url  = obj["url"] as String
+//                    val photos = Photos(albumId,id, title, url)
                     Log.i("Networking Response", photos.toString())
 
                 }
---
+++ Lecture9/app/src/main/java/com/puldroid/networking/PhotoAdapter.kt
@@ -0,0 +1,44 @@
+package com.puldroid.networking
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.CornerFamily
+import com.squareup.picasso.Picasso
+
+/**
+ * @author aggarwalpulkit596
+ */
+
+class PhotoAdapter(val photos: List<Photos>) : RecyclerView.Adapter<PhotosViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        PhotosViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_photo,
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount() = photos.size
+
+    override fun onBindViewHolder(holder: PhotosViewHolder, position: Int) {
+        holder.bind(photos[position])
+    }
+
+}
+
+class PhotosViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(photos: Photos) = with(itemView as ShapeableImageView){
+        Picasso.get().load(photos.url).placeholder(R.drawable.ic_launcher_background).into(this)
+        shapeAppearanceModel = shapeAppearanceModel.toBuilder()
+            .setBottomRightCorner(CornerFamily.CUT, 100f)
+            .setBottomLeftCorner(CornerFamily.CUT, 100f)
+            .setTopLeftCorner(CornerFamily.ROUNDED, 100f)
+            .setTopRightCorner(CornerFamily.ROUNDED, 100f)
+            .build()
+
+    }
+}
