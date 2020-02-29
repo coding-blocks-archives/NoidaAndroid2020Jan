b24607c5864bf2b2e7c78eb25c69ed70d14a86eb
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-02-29 18:48:21 +0530

    add menu to toolbar

--- Lecture12/app/src/main/java/com/puldroid/misc/ToolbarActivity.kt
+++ Lecture12/app/src/main/java/com/puldroid/misc/ToolbarActivity.kt
@@ -1,7 +1,10 @@
 package com.puldroid.misc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_toolbar.*
 
 class ToolbarActivity : AppCompatActivity() {
@@ -12,6 +15,35 @@ class ToolbarActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
+            setTitle("Hello world this is a toolbar")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.first ->{
+            Toast.makeText(this,"Simple Toast from Menu",Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.first1 ->{
+            true
+        }
+        R.id.first2 ->{
+            true
+        }
+        R.id.first3 ->{
+            true
+        }
+        android.R.id.home ->{
+            //Custom Back Button Action
+            Toast.makeText(this,"Simple Toast",Toast.LENGTH_SHORT).show()
+            true
+        }
+        else ->  super.onOptionsItemSelected(item)
+
     }
 }
