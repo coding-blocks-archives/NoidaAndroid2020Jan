14263a42d5ade4693152f3ef02abf55446b3c4eb
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-03-01 17:30:45 +0530

    changing visibility of fragments by code

--- Lecture13/app/src/main/java/com/puldroid/fragments/DcFragment.kt
+++ Lecture13/app/src/main/java/com/puldroid/fragments/DcFragment.kt
@@ -4,33 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_blank.*
 
-class BlankFragment : Fragment() {
-
-    val list = arrayListOf("Iron Man","Thor","Doctor Strange","Marvel","Hulk")
-     val adapter = MovieAdapter()
+class DcFragment : Fragment() {
+    val list = arrayListOf("Superman", "Batman", "Wonder Woman", "Flash", "Arrow")
+    val adapter = MovieAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View?  = inflater.inflate(R.layout.fragment_blank, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_blank, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         adapter.swapData(list)
         moviesRv.apply {
             layoutManager = LinearLayoutManager(requireContext())
-            adapter = this@BlankFragment.adapter
+            adapter = this@DcFragment.adapter
         }
         button.setOnClickListener {
-            Toast.makeText(requireContext(),"Simple Toast",Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(), "Simple Toast", Toast.LENGTH_LONG).show()
         }
     }
-
 }
--- Lecture13/app/src/main/java/com/puldroid/fragments/MainActivity.kt
+++ Lecture13/app/src/main/java/com/puldroid/fragments/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.puldroid.fragments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.simple_layout.*
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        base2fragment.view?.visibility = View.GONE
+
+        button2.setOnClickListener {
+            if (base2fragment.view?.visibility == View.VISIBLE) {
+                base2fragment.view?.visibility = View.GONE
+                baseFragment.view?.visibility = View.VISIBLE
+            } else {
+                baseFragment.view?.visibility = View.GONE
+                base2fragment.view?.visibility = View.VISIBLE
+            }
+        }
+
 
     }
 }
