45cb33c9b9b2076fa8890162213f0820fe7f6070
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-03-21 17:58:37 +0530

    write in background thread

--- Lecture16/app/src/main/java/com/pulkit/mediaplayer/MainActivity.kt
+++ Lecture16/app/src/main/java/com/pulkit/mediaplayer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pulkit.mediaplayer
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
+import android.os.StrictMode
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -23,6 +24,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                        .detectAll()
+                        .penaltyDeath()
+                        .build()
+        )
 
         button.setOnClickListener {
             val perm = ActivityCompat.checkSelfPermission(
@@ -56,14 +64,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun restoreFile() {
+
         Toast.makeText(this, "Reading form a file", Toast.LENGTH_SHORT).show()
-        val directory = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath
-        val file = File(directory,"data.txt")
-        if(file.exists()){
-            val savedData = file.readText()
-            editText.setText(savedData)
-        }else{
-            Toast.makeText(this, "Nothing to restore", Toast.LENGTH_SHORT).show()
+        GlobalScope.launch {
+            val directory = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath
+            val file = File(directory, "data.txt")
+            if (file.exists()) {
+                val savedData = withContext(Dispatchers.IO) { file.readText() }
+                GlobalScope.launch(Dispatchers.Main) {
+                    editText.setText(savedData)
+                }
+            } else {
+                GlobalScope.launch(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Nothing to restore", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
 
     }
@@ -71,33 +86,32 @@ class MainActivity : AppCompatActivity() {
     private fun saveToFile() {
         Toast.makeText(this, "Writing to a file", Toast.LENGTH_SHORT).show()
         val data = editText.text.toString()
-        val directory = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath
+        GlobalScope.launch {
 
-        Log.i("Download file Path",directory)
-
-        val file = File(directory,"data.txt")
-        GlobalScope.launch{
-            val fileWritten = withContext(Dispatchers.IO){
+            val directory = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath
+            Log.i("Download file Path", directory)
+            val file = File(directory, "data.txt")
+            val fileWritten = withContext(Dispatchers.IO) {
                 file.writeBytes(data.toByteArray())
                 true
             }
-            Log.i("Download file Path",fileWritten.toString())
+            Log.i("Download file Path", fileWritten.toString())
         }
 
 
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if(requestCode == WRITE_PER_CODE){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == WRITE_PER_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 saveToFile()
-            }else{
+            } else {
                 Toast.makeText(this, "Cannot write to a file", Toast.LENGTH_SHORT).show()
             }
-        }else  if(requestCode == READ_PER_CODE){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        } else if (requestCode == READ_PER_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 restoreFile()
-            }else{
+            } else {
                 Toast.makeText(this, "Cannot read a file", Toast.LENGTH_SHORT).show()
             }
         }
