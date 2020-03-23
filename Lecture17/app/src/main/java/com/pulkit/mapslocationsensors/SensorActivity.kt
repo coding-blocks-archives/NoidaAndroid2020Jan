b047fda1546bb1e77b6b18072cf2d456c28cdc2a
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-03-23 12:45:45 +0530

    disco lights using accelerometer

--- Lecture17/app/src/main/java/com/pulkit/mapslocationsensors/SensorActivity.kt
+++ Lecture17/app/src/main/java/com/pulkit/mapslocationsensors/SensorActivity.kt
@@ -1,6 +1,7 @@
 package com.pulkit.mapslocationsensors
 
 import android.content.Context
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -8,11 +9,13 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sensor.*
 
 class SensorActivity : AppCompatActivity() {
     lateinit var sm: SensorManager
     lateinit var sensorEventListener: SensorEventListener
-    lateinit var proximitySensor:Sensor
+    lateinit var proximitySensor: Sensor
+    lateinit var accSensor: Sensor
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -30,23 +33,37 @@ class SensorActivity : AppCompatActivity() {
             )
         }
         proximitySensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        accSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         sensorEventListener = object : SensorEventListener {
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
                 Log.d("SENSOR", "onAccuracyChanged : $accuracy")
             }
 
             override fun onSensorChanged(event: SensorEvent?) {
-                Log.d("SENSOR", "onSensorChanged : ${event?.values?.get(0)}")
+                event?.values?.let {
+                    val bgColor = accelToColor(it[0], it[1], it[2])
+                    bgLayout.setBackgroundColor(bgColor)
+                }
             }
         }
+    }
+
+    private fun accelToColor(ax: Float, ay: Float, az: Float): Int {
+        // -12 to +12
+        val R = (((ax + 12) / 24) * 255).toInt()
+        val G = (((ay + 12) / 24) * 255).toInt()
+        val B = (((az + 12) / 24) * 255).toInt()
+
+        return Color.rgb(R,G,B)
+
     }
 
     override fun onResume() {
         super.onResume()
         sm.registerListener(
             sensorEventListener,
-            proximitySensor,
-            1000 * 1000
+            accSensor,
+            100 * 100
         )
     }
 
