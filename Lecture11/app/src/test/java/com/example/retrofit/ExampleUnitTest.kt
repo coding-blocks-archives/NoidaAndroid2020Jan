b82fbda7e4060bd95e47f05401b5ed120cd5a5d6
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-04-05 13:55:57 +0530

    Add Unit testing for API

--- Lecture11/app/src/test/java/com/example/retrofit/ExampleUnitTest.kt
+++ Lecture11/app/src/test/java/com/example/retrofit/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.retrofit
 
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,8 +11,35 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun checkUser() {
+        runBlocking {
+            val response = Client.api.getMyUser()
+            assertNotNull(response.body())
+            assertEquals(30,response.body()?.size)
+        }
     }
+
+
+    @Test
+    fun checkMyUser() {
+        runBlocking {
+            val response = Client.api.getUser("aggarwalpulkit596")
+            assertNotNull(response.body())
+            assertEquals("aggarwalpulkit596",response.body()?.login)
+        }
+    }
+
+    @Test
+    fun findUser() {
+        runBlocking {
+            val response = Client.api.searchUser("aggarwalpulkit596")
+            assertNotNull(response.body())
+            assertNotEquals(0,response.body()?.items?.size)
+            assertEquals("aggarwalpulkit596", response.body()?.items?.get(0)?.login)
+        }
+    }
+
+
 }
